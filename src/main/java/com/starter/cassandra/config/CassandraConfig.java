41bd162e970c986cc0bdb8c51bc33373479e754e
barjinderpaul <barjinder.singh@magicpin.in>
2020-02-18 01:12:27 +0530

    Add Cassandra config

---
+++ src/main/java/com/starter/cassandra/config/CassandraConfig.java
@@ -0,0 +1,60 @@
+package com.starter.cassandra.config;
+
+import com.datastax.driver.core.AuthProvider;
+import com.datastax.driver.core.PlainTextAuthProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+public class CassandraConfig extends AbstractCassandraConfiguration {
+    @Value("${spring.data.cassandra.contact-points:localhost}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.port:9042}")
+    private int port;
+
+    @Value("${spring.data.cassandra.keyspace:users}")
+    private String keySpace;
+
+    @Value("${spring.data.cassandra.username}")
+    private String username;
+
+    @Value("${spring.data.cassandra.password}")
+    private String password;
+
+    @Value("${spring.data.cassandra.schema-action}")
+    private String schemaAction;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keySpace;
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.valueOf(schemaAction);
+    }
+
+    @Override
+    protected AuthProvider getAuthProvider() {
+        return new PlainTextAuthProvider(username, password);
+    }
+
+    @Override
+    protected boolean getMetricsEnabled() { return false; }
+
+}
